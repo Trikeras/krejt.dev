8a1609580f432d62c15cb57258e8b26d2a82bd56
Arbrim Rashiti <arbrimrashiti@gmail.com>
2022-04-28 22:05:43 +0200

    More message box width on phone. Message height not fixed on both

--- src/page/about-us.js
+++ src/page/about-us.js
@@ -12,14 +12,15 @@ class AboutUs extends React.Component {
   render() {
 
     const markdown = `
-      ### Mire se vini zhvillues
+# Mire se vini zhvillues
 
-      **Krejt.dev** eshte komunitet per krejt zhvilluesit:
-      programera, dizajnera, muzikant, projekt menaxhera,
-      etj.
+**Krejt.dev** eshte komunitet per krejt zhvilluesit: programera, dizajnera, muzikant, projekt menaxhera etj.
 
-      Njerzt qe punojne edhe pas ores 6
-      `;
+Qellimi i krejt neve eshte qe te nxitet bashkepunimi, krejt kjo duke e lehtesu komunikimin mes krejtve.
+Krejt do te kemi slack channels per cfardo lemi qe na intereson si per shembull: java, nodejs, kubernetes, react, javacsript, business, events, applications, design dhe cfardoe tjeter qe do te na lehtesonte komunikimin kshtuqe edhe bashkepunimin mes nesh.
+
+Njerzt qe punojne edhe pas ores 6
+`;
 
 
     return (
